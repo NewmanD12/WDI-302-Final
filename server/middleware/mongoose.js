94108b78687b11d31b73dfcb9e30c19ca0c13507
Dakota Newman <dakota.newman@yahoo.com>
2023-08-11 19:01:55 -0400

    initiated change to make sure everything is up to date

--- server/middleware/mongoose.js
+++ server/middleware/mongoose.js
@@ -7,7 +7,6 @@ const mongoDB = process.env.ATLAS_URI
 async function mongooseConnect() {
     try {
         await mongoose.connect(mongoDB, {dbName : process.env.DATABASE})
-        console.log('db connected')
     }   
     catch (err) {
         throw(err)
