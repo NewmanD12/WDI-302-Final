1f6a0c31c3fd6c85cda006ade955d051dd8a23bf
Dakota Newman <dakota.newman@yahoo.com>
2023-08-11 21:03:55 -0400

    reconfigged package.json and deleted axios middleware

--- client/src/lib/Axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const Axios = axios.create({
-    baseURL: process.env.REACT_APP_AXIOS === 'development' ? 'http://localhost:3001/api' : '/api',
-    timeout: 50000
-})
-
-export default Axios;
