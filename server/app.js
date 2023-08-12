c0c8e487ceae2c5d747f3014004ae1adbc9b1268
Dakota Newman <dakota.newman@yahoo.com>
2023-08-11 21:13:43 -0400

    swapped cors options and cors use to troubleshoot

--- server/app.js
+++ server/app.js
@@ -28,8 +28,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors());
 app.options("*", cors());
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
